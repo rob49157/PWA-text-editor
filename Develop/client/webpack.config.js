bd51fe0bd849731d65b7d7b2bb05615c0b2ea257
rob49157 <robertopaipo@hotmail.com>
2022-01-17 21:11:22 -0500

    package.json

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+// import css from '../client/src/css'
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,7 +19,9 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // HtmlWebpackPlugin
+      // InjectManifest
+      // WebpackPwaManifest
     ],
 
     module: {
@@ -33,7 +36,8 @@ module.exports = () => {
       use: {
         loader: 'babel-loader',
         options:{
-          present:['@babel/preset-env']
+          presets:['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
         }
         },
       }
