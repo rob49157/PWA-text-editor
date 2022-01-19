56d6dda7d3dea5e973203079acd73b47b733fdf5
rob49157 <robertopaipo@hotmail.com>
2022-01-18 19:12:46 -0500

    some house cleaning

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,62 +13,62 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
 
-    // Create a connection to the database database and version we want to use.
+
   const contactDb = await openDB('J.A.T.E', 1);
 
-  // Create a new transaction and specify the database and data privileges.
+
   const tx = contactDb.transaction('J.A.T.E', 'readwrite');
 
-  // Open up the desired object store.
+
   const store = tx.objectStore('J.A.T.E');
 
-  // Use the .add() method on the store and pass in the content.
-  // const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
 
-  // Get confirmation of the request.
   const result = await request;
-  console.log('🚀 - data saved to the database', result.value);}
+  console.log('🚀 - data saved to the database', result.value);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {console.error('getDb not implemented')
-// Create a connection to the database database and version we want to use.
-const contactDb = await openDB('J.A.T.E', 1);
+export const getDb = async () => {
+  console.error('getDb not implemented')
 
-// Create a new transaction and specify the database and data privileges.
-const tx = contactDb.transaction('J.A.T.E', 'readonly');
+  const contactDb = await openDB('J.A.T.E', 1);
 
-// Open up the desired object store.
-const store = tx.objectStore('J.A.T.E');
 
-// Use the .getAll() method to get all data in the database.
-const request = store.getAll();
+  const tx = contactDb.transaction('J.A.T.E', 'readonly');
 
-// Get confirmation of the request.
-const result = await request;
-console.log('result.value', result);
-return result.value}
-;
+
+  const store = tx.objectStore('J.A.T.E');
+
+  const request = store.getAll();
+
+
+  const result = await request;
+  console.log('result.value', result);
+  return result.value
+}
+  ;
 
 
 // Delete
 export const deleteDb = async (id) => {
   console.log('DELETE from the database', id);
 
-  // Create a connection to the database database and version we want to use.
+
   const contactDb = await openDB('J.A.T.E', 1);
 
-  // Create a new transaction and specify the database and data privileges.
+
   const tx = contactDb.transaction('J.A.T.E', 'readwrite');
 
-  // Open up the desired object store.
+
   const store = tx.objectStore('J.A.T.E');
 
-  // Use the .delete() method to get all data in the database.
+
   const request = store.delete(id);
 
-  // Get confirmation of the request.
+
   const result = await request;
   console.log('result.value', result);
   return result?.value;
